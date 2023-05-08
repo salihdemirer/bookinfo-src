b728bc089b107eb4190e0cc8d02ad0f250abbe92
Salih Furkan Demirer <sfdemirer333@outlook.com>
2023-05-08 19:23:15 +0300

    Update setup.sh

--- productpage/setup.sh
+++ productpage/setup.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 pip3 install -U pip
-git clone https://github.com/salihdemirer/bookinfo-src.git
 cd bookinfo-src/productpage/
 pip3 install -r requirements.txt
 screen python3 productpage.py 9080
